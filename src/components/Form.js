48875ca231b79034ac114850c85fbcded0e422a2
Tom Pocius <poci14@gmail.com>
2020-02-17 22:39:07 -0600

    added apicall for home

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = props => {
   return (
-    <form onSubmit={props.onClick}>
+    <form onSubmit={props.submitform}>
       <input type="text" placeholder="search" name="searchQuery"></input>
     </form>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import API from '../utils/API';
+import Form from '../components/Form';
 
 const Home = () => {
-  return <div className="frow">hello Home</div>;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchMovies = query => {
+    API.search(query)
+      .then(res => setResults(res.data))
+      .catch(err => console.log(err));
+  };
+
+  const handleInput = event => {
+    event.preventDefault();
+    console.log('input', event.target.searchQuery.value);
+    setSearchQuery(event.target.searchQuery.value);
+    searchMovies(event.target.searchQuery.value);
+  };
+
+  console.log(results);
+
+  return (
+    <>
+      <div className="frow">hello Home</div>
+      <Form submitform={handleInput}></Form>
+    </>
+  );
 };
 
 export default Home;
