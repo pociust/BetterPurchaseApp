eb43d3b5ebe113e5f06a05f2e94aede38e4c4a93
Tom Pocius <poci14@gmail.com>
2020-02-17 22:03:56 -0600

    added axios and frow to heading in home and cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Cart = () => {
-  return <div>this is a cart page</div>;
+  return <div className="frow">this is a cart page</div>;
 };
 
 export default Cart;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>hello Home</div>;
+  return <div className="frow">hello Home</div>;
 };
 
 export default Home;
