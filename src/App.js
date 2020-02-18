06afbcdbe261fb20633eb350d52c2778526519a0
Tom Pocius <poci14@gmail.com>
2020-02-17 21:16:05 -0600

    created router and home and cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    helloworld
-    </div>
+    <>
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/cart">My Cart</Link>
+        </div>
+
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/cart">
+            <Cart></Cart>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
